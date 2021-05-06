5a873fdaddd4ba9e6784f870c19d1cc06de0d1d9
Sunny <shubelislam.diu@gmail.com>
2021-05-06 20:34:13 +0600

    null vs undefined is cleared for final job interview

--- null-vs-undefined.js
+++ null-vs-undefined.js
@@ -1,20 +1,20 @@
-// let bird;
+let bird;
 
-// function add(num1, num2) {
-//     console.log(num1 + num2);
-//     return
-// }
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return
+}
 
-// function add(num1, num2) {
-//     console.log(num1, num2);
-//     return
-// }
+function add(num1, num2) {
+    console.log(num1, num2);
+    return
+}
 
-// const premik = {name:"smart dude", number:243535};
-// console.log(premik.gf);
+const premik = {name:"smart dude", number:243535};
+console.log(premik.gf);
 
-// let fun = "undefined";
-// console.log(fun);
+let fun = "undefined";
+console.log(fun);
 
 let ages = [12, 33, 54];
 console.log(ages[29]);
