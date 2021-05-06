6924302902ece0574170fd1983634ef5f125676e
Sunny <shubelislam.diu@gmail.com>
2021-05-06 20:27:11 +0600

    numm vs undefined cleared for interview

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+// let bird;
+
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return
+// }
+
+// function add(num1, num2) {
+//     console.log(num1, num2);
+//     return
+// }
+
+// const premik = {name:"smart dude", number:243535};
+// console.log(premik.gf);
+
+// let fun = "undefined";
+// console.log(fun);
+
+let ages = [12, 33, 54];
+console.log(ages[29]);
